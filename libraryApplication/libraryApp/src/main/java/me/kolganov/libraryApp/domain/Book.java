122df3a84ef72ae7101f6888a9216f461c7114f2
roman <ramoni1990@gmail.com>
2019-09-29 21:50:58 +0300

    Мапинг сущностей

--- libraryApplication/libraryApp/src/main/java/me/kolganov/libraryApp/domain/Book.java
+++ libraryApplication/libraryApp/src/main/java/me/kolganov/libraryApp/domain/Book.java
@@ -1,9 +1,20 @@
 package me.kolganov.libraryApp.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @OneToOne(targetEntity = Author.class)
+    @JoinColumn(name = "author_id")
     private Author author;
+    @OneToOne(targetEntity = Genre.class)
+    @JoinColumn(name = "genre_id")
     private Genre genre;
 
     public Book(long id, String name, Author author, Genre genre) {
