a17d2733bdb15c1c7c699859f2ac3a4a474593d5
roman <ramoni1990@gmail.com>
2019-10-28 22:00:27 +0300

    Убрал лишние методы из репозиториев

--- spring-MVC-view (hw-9)/src/main/java/me/kolganov/springmvcview/dao/AuthorDAO.java
+++ spring-MVC-view (hw-9)/src/main/java/me/kolganov/springmvcview/dao/AuthorDAO.java
@@ -3,8 +3,5 @@ package me.kolganov.springmvcview.dao;
 import me.kolganov.springmvcview.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface AuthorDAO extends JpaRepository<Author, Long> {
-    Optional<Author> findById(long id);
 }
--- spring-MVC-view (hw-9)/src/main/java/me/kolganov/springmvcview/dao/CommentDAO.java
+++ spring-MVC-view (hw-9)/src/main/java/me/kolganov/springmvcview/dao/CommentDAO.java
@@ -4,9 +4,7 @@ import me.kolganov.springmvcview.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CommentDAO extends JpaRepository<Comment, Long> {
-    Optional<Comment> findById(long id);
     List<Comment> findAllByBookId(long bookId);
 }
