499aef44f0b41dfcdf62936b8bcab552401449bb
roman <ramoni1990@gmail.com>
2019-11-07 00:36:19 +0400

    Убрал лишнее в авторе

--- spring-mvc-view-ajax-hw-10/src/main/java/me/kolganov/springmvcview/page/AuthorPageController.java
+++ spring-mvc-view-ajax-hw-10/src/main/java/me/kolganov/springmvcview/page/AuthorPageController.java
@@ -1,30 +1,12 @@
 package me.kolganov.springmvcview.page;
 
-import me.kolganov.springmvcview.domain.Author;
-import me.kolganov.springmvcview.service.AuthorService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthorPageController {
-    private final AuthorService authorService;
-
-    public AuthorPageController(AuthorService authorService) {
-        this.authorService = authorService;
-    }
-
     @GetMapping(value = "/authors")
     public String getAll() {
         return "author";
-    }
-
-    @GetMapping(value = "/authors/edit")
-    public String getOne(@RequestParam("id") long id, Model model) {
-        Author author = authorService.getById(id);
-        model.addAttribute("author", author);
-        return "authorEdit";
     }
 }
