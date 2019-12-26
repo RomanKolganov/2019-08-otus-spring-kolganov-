5ece7269fde989c078e653ed1e9c751520b08484
roman <ramoni1990@gmail.com>
2019-12-26 22:57:57 +0300

    Подключил актуатор на все эндпоинты

--- spring-actuator/src/main/java/me/kolganov/springactuator/SpringActuatorApplication.java
+++ spring-actuator/src/main/java/me/kolganov/springactuator/SpringActuatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityAclApplication {
+public class SpringActuatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityAclApplication.class, args);
+		SpringApplication.run(SpringActuatorApplication.class, args);
 	}
 
 }
